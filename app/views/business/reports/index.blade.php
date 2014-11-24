20108b7b8ea60c55330bf2b21dd0947a5a614e55
Pedro Camargo <dropecamargo@gmail.com>
2014-11-23 21:37:17 -0500

    Reportes - GNP

--- app/views/business/reports/index.blade.php
+++ app/views/business/reports/index.blade.php
@@ -18,9 +18,24 @@
 				</div>
 				<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
 					<div class="panel-body">
-						{{ Form::open(array('url' => array('business/reports/carteraedades'), 'method' => 'POST'), array('role' => 'form')) }}			
-							{{ Form::submit('Cartera vencida por edades', array('class' => 'btn btn-info')) }}
-						{{ Form::close() }}
+						{{ Form::open(array('url' => array('business/reports/carteraedades'), 'method' => 'POST'), array('role' => 'form')) }}									
+							<div class="row" align="center">
+								<div class="form-group col-md-3"></div>
+								<div class="form-group col-md-6">
+								{{ Form::label('edades', 'Edad Cartera') }}						
+								{{ Form::select('edades', array(
+									'T' => 'Todas', '30' => '1 A 30', 
+									'60' => '31 A 60', '90' => '61 A 90',
+									'180' => '91 A 180', '360' => '181 A 360',
+									'M360' => 'MAS DE 360'),'T',array('class' => 'form-control', 'style' => 'width:30;')) 
+								}}
+								</div>
+								<div class="form-group col-md-3"></div>
+							</div>
+							<p align="center">
+								{{ Form::submit('Generar', array('class' => 'btn btn-info')) }}
+							</p>
+						{{ Form::close() }}	
 					</div>
 				</div>
 			</div>
@@ -34,7 +49,24 @@
 				</div>
 				<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
 					<div class="panel-body">
-						Estado de cuenta
+						{{ Form::open(array('url' => array('business/reports/estadocuenta'), 'method' => 'POST'), array('role' => 'form')) }}									
+							<div class="row" align="center">
+								<div class="form-group col-md-1"></div>
+								<div class="form-group col-md-3">
+									{{ Form::label('cliente_cedula', 'Cliente') }}
+						           	{{ Form::text('cliente_cedula', null, array('placeholder' => 'Ingrese cliente', 'class' => 'form-control')) }}        
+						        	{{ Form::hidden('cliente', null, array('id' => 'cliente')) }}
+								</div>
+								<div class="form-group col-md-7">   
+									{{ Form::label('cliente_nombre', 'Nombre') }}     	
+						            {{ Form::text('cliente_nombre', null, array('class' => 'form-control', 'disabled' => 'disabled')) }}        
+						        </div>
+								<div class="form-group col-md-1"></div>
+							</div>
+							<p align="center">
+								{{ Form::submit('Generar', array('class' => 'btn btn-info')) }}
+							</p>
+						{{ Form::close() }}
 					</div>
 				</div>
 			</div>
@@ -48,7 +80,22 @@
 				</div>
 				<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
 					<div class="panel-body">
-						Recibos de caja
+						{{ Form::open(array('url' => array('business/reports/reciboscaja'), 'method' => 'POST'), array('role' => 'form')) }}									
+							<div class="row" align="center">
+								<div class="form-group col-md-3"></div>
+								<div class="form-group col-md-6">
+								{{ Form::label('tipo', 'Tipo') }}
+								{{ Form::select('tipo', array(
+									'T' => 'Todos', 'PA' => 'Pago', 'DE' => 'Descuento', 'DV' => 'Devolución'),
+									'T',array('class' => 'form-control', 'style' => 'width:30;')) 
+								}}
+								</div>
+								<div class="form-group col-md-3"></div>
+							</div>
+							<p align="center">
+								{{ Form::submit('Generar', array('class' => 'btn btn-info')) }}
+							</p>
+						{{ Form::close() }}
 					</div>
 				</div>
 			</div>
@@ -62,7 +109,27 @@
 				</div>
 				<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
 					<div class="panel-body">
-						Ventas de un periodo
+						{{ Form::open(array('url' => array('business/reports/ventas'), 'method' => 'POST'), array('role' => 'form')) }}									
+							<div class="row" align="center">
+								<div class="form-group col-md-2"></div>
+								<div class="form-group col-md-4">
+									{{ Form::label('fecha_inicial', 'Fecha Inicial') }}
+						            <div class="input-append date">	
+						            	{{ Form::text('fecha_inicial', null, array('placeholder' => 'yyyy-mm-dd', 'class' => 'form-control')) }}        
+						        	</div>
+								</div>
+								<div class="form-group col-md-4">
+									{{ Form::label('fecha_final', 'Fecha Final') }}
+						            <div class="input-append date">	
+						            	{{ Form::text('fecha_final', null, array('placeholder' => 'yyyy-mm-dd', 'class' => 'form-control')) }}        
+						        	</div>
+								</div>
+								<div class="form-group col-md-2"></div>
+							</div>
+							<p align="center">
+								{{ Form::submit('Generar', array('class' => 'btn btn-info')) }}
+							</p>
+						{{ Form::close() }}
 					</div>
 				</div>
 			</div>
@@ -70,6 +137,64 @@
 
 		<script type="text/javascript">
 			$(function() {	
+				var root_url = "<?php echo Request::root(); ?>/";
+
+				$("#fecha_inicial").datepicker({
+	                changeMonth: true,
+	                changeYear: true,
+	                dateFormat: "yy-mm-dd"              
+            	})
+
+            	$("#fecha_final").datepicker({
+	                changeMonth: true,
+	                changeYear: true,
+	                dateFormat: "yy-mm-dd"              
+            	})
+
+            	var event_client = function() {
+				var inputVal = $("#cliente_cedula").val();
+			    var numericReg = /^\d*[0-9](|.\d*[0-9]|,\d*[0-9])?$/;
+			    
+			    $('#cliente').val('')
+        		$('#cliente_nombre').val('')
+
+			    if(!numericReg.test(inputVal)) {
+			    	$("#cliente_cedula").val('')
+			    }else{
+			    	var url = root_url + 'business/customers/find';
+			    	$.ajax({
+		                type: 'post',
+		                cache: false,
+		                dataType: 'json',
+		                data: { cedula : inputVal },
+		                url : url,
+		                beforeSend: function() {
+							$('#loading-app').modal('show');
+						},
+		                success: function(data) {
+		                	$('#loading-app').modal('hide')
+		                	if(data.success == false) {
+								$('#cedula').val(inputVal)									
+		                	}else{		                	
+		                		$('#cliente').val(data.customer.id)
+	                    		$('#cliente_cedula').val(data.customer.cedula)
+	                    		$('#cliente_nombre').val(data.customer.nombre)	
+		                	}
+		                	$('#modal-client').modal('show')		                	
+		                },
+		                error: function(xhr, textStatus, thrownError) {
+							$('#loading-app').modal('hide');
+							$('#error-app').modal('show');
+							$("#error-app-label").empty().html("No hay respuesta del servidor - Consulte al administrador.");				
+		                }
+		            });											
+				}				
+			};
+
+
+			$("#cliente_cedula").change(function() {
+				event_client();	
+			});
 
 			});
 		</script>
